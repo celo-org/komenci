71c30d5210fb71b392a45b4f4f19ff6fb9531f5b
Bogdan Dumitru <bogdan@bowd.io>
2020-09-02 08:31:39 +0200

    Streamline microservice init

--- apps/relayer/src/main.ts
+++ apps/relayer/src/main.ts
@@ -3,21 +3,22 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { TcpOptions, Transport } from '@nestjs/microservices';
 import { ServiceConfig } from 'apps/relayer/src/config/service.config';
-import { createSecureServer } from 'http2';
 import { AppModule } from './app.module';
 
 const logger = new Logger("bootstrap");
 
 async function bootstrap() {
-  const ctx = await NestFactory.createApplicationContext(AppModule)
-  const serviceConfig = ctx.get(ConfigService).get<ServiceConfig>("service")
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.create(AppModule)
+  const serviceConfig = app.get(ConfigService).get<ServiceConfig>("service")
+  app.connectMicroservice<TcpOptions>({
     transport: Transport.TCP,
     options: {
       host: serviceConfig.host,
       port: serviceConfig.port,
     }
   })
-  await app.listen(() => logger.log(`Microservice is listening on ${serviceConfig.host}:${serviceConfig.port}`));
+  await app.startAllMicroservices(() => {
+    logger.log(`Microservice is listening on ${serviceConfig.host}:${serviceConfig.port}`)
+  });
 }
 bootstrap();
