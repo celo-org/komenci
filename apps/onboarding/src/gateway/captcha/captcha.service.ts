34237b7667b5e7f653495e408fe5c011d9c7b4f6
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-09-07 15:19:24 +0200

    Verify reCaptcha tokens

--- apps/onboarding/src/gateway/captcha/captcha.service.ts
+++ apps/onboarding/src/gateway/captcha/captcha.service.ts
@@ -1,5 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import fetch from 'node-fetch';
+
 import thirdPartyConfig from 'apps/onboarding/src/config/third-party.config';
 
 @Injectable()
@@ -9,9 +11,11 @@ export class CaptchaService {
     private config: ConfigType<typeof thirdPartyConfig>
   ) {}
 
-  // TODO determine what the propper input is for this
-  async verifyCaptcha(input: any): Promise<boolean> {
-    // this.config.recaptchaToken => would hold a token
-    return false
+  async verifyCaptcha(input: {token: string}): Promise<boolean> {
+    const verifyUrl = `https://www.google.com/recaptcha/api/siteverify?secret=${this.config.recaptchaToken}&response=${input.token}`
+    const response = await fetch(verifyUrl)
+    const {success, 'error-codes': errors} = await response.json()
+
+    return success
   }
 }
