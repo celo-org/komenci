b6d8a752bd735173f933a9e00e3ee87bb7b04fe0
Pedro Gutiérrez <pedrogp9693@gmail.com>
2020-09-09 14:40:49 +0200

    Test reCaptcha service correctly

--- apps/onboarding/src/gateway/captcha/captcha.service.spec.ts
+++ apps/onboarding/src/gateway/captcha/captcha.service.spec.ts
@@ -1,10 +1,11 @@
-import { HttpModule } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CaptchaService } from './captcha.service';
 import { AppModule } from '../../app.module';
 
 describe('CaptchaService', () => {
   let service: CaptchaService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +14,20 @@ describe('CaptchaService', () => {
     }).compile();
 
     service = module.get<CaptchaService>(CaptchaService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should verify the token', async() => {
+    const httpServiceSpy = spyOn(httpService, 'get').and.returnValue({toPromise: () => ({data: 'test'})})
+
+    expect(await service.verifyCaptcha({token: 'token-test'})).toEqual('test')
+
+    expect(httpServiceSpy).toHaveBeenCalledWith(expect.any(String), {params: expect.objectContaining({
+      response: 'token-test',
+    })})
   });
 });
