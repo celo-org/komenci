5d0f148e6163fb7b2b58cb15e94b32b50c22dc81
enrique <enrique@keyko.io>
2020-09-16 11:17:36 +0200

    Implement ios device check api

---
+++ apps/onboarding/src/dto/DeviceCheckDto.ts
@@ -0,0 +1,5 @@
+export interface DeviceCheckDto{
+    //https://developer.apple.com/documentation/devicecheck/accessing_and_modifying_per-device_data#2910408
+    // Not clear which response we are going to obtain from the server
+    isValidSignature: boolean
+}
