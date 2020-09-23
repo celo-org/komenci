9a0a308b360058c15c5fbf466e11d7033366ed14
enrique <enrique@keyko.io>
2020-09-23 12:21:41 +0200

    Refactor config

--- apps/onboarding/src/session/session.service.spec.ts
+++ apps/onboarding/src/session/session.service.spec.ts
@@ -28,21 +28,17 @@ describe('SessionService', () => {
   it('should create a session object', async () => {
     const session = Session.of({externalAccount:'test', id: '1'})
 
-    const createSessionSpy = jest.spyOn(service, 'createSession').mockResolvedValue(session)
+    jest.spyOn(service, 'createSession').mockResolvedValue(session)
 
-    const res = await service.createSession('test')
-
-    expect(res).toBe(session)
+    expect(await service.createSession('test')).toBe(session)
 
     jest.spyOn(service, 'findOne').mockResolvedValue(session)
 
-    const r = await service.findOne(session.id)
-    expect(r).toBe(session)
+    expect(await service.findOne(session.id)).toBe(session)
 
     jest.spyOn(service, 'findAll').mockResolvedValue([session])
 
-    const re = await service.findAll()
-    expect(re.length).toBe(1)
+    expect((await service.findAll()).length).toBe(1)
 
   })
 
