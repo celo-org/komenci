637445119d6de4498400f6e91569d6f73b0a6df6
Bogdan Dumitru <bogdan@bowd.io>
2020-09-29 17:14:19 +0200

    Showcase how config could be changed for each spec

--- apps/onboarding/src/gateway/gateway.service.spec.ts
+++ apps/onboarding/src/gateway/gateway.service.spec.ts
@@ -5,65 +5,141 @@ import { DeviceCheckService } from 'apps/onboarding/src/gateway/device-check/dev
 import { RuleID } from 'apps/onboarding/src/gateway/rules/rule';
 import { SafetyNetService } from 'apps/onboarding/src/gateway/safety-net/safety-net.service';
 import appConfig from 'apps/relayer/src/config/app.config';
-import rulesConfig from '../config/rules.config'
+import rulesConfig, { RulesConfig } from '../config/rules.config'
 import thirdPartyConfig from '../config/third-party.config'
 import { CaptchaService } from './captcha/captcha.service'
 import { GatewayService } from './gateway.service'
 
 describe('GatewayService', () => {
 
-  const buildTestingModule = async (): Promise<TestingModule> => {
-    return Test.createTestingModule({
-      imports: [
-        HttpModule,
-        ConfigModule.forRoot({
-          load: [thirdPartyConfig, rulesConfig],
-        })
-      ],
-      providers: [
-        GatewayService,
-        CaptchaService,
-        DeviceCheckService,
-        SafetyNetService,
-      ]
-    }).compile()
-  }
+  describe("by changing the env", () => {
+    const buildTestingModule = async (): Promise<TestingModule> => {
+      return Test.createTestingModule({
+        imports: [
+          HttpModule,
+          ConfigModule.forRoot({
+            load: [thirdPartyConfig, rulesConfig],
+          })
+        ],
+        providers: [
+          GatewayService,
+          CaptchaService,
+          DeviceCheckService,
+          SafetyNetService,
+        ]
+      }).compile()
+    }
 
-  it('should be defined', async () => {
-    const module = await buildTestingModule()
-    const service = module.get<GatewayService>(GatewayService)
-    expect(service).toBeDefined()
+    it('should be defined', async () => {
+      const module = await buildTestingModule()
+      const service = module.get<GatewayService>(GatewayService)
+      expect(service).toBeDefined()
+    })
+
+    it('should disable the rules depending on env variables when empty', async () => {
+      process.env.RULE_DAILY_CAP_ENABLED = ''
+      const module = await buildTestingModule()
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleEnabled).toEqual(expect.objectContaining({ DAILY_CAP: false }))
+    })
+
+    it('should enable the rules depending on env variables when true', async () => {
+      process.env.RULE_DAILY_CAP_ENABLED = 'true'
+      const module = await buildTestingModule()
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleEnabled).toEqual(expect.objectContaining({DAILY_CAP: true}))
+    })
+
+    it('should set the rules config depending on env variables when empty', async () => {
+      process.env.RULE_CAPTCHA_CONFIG = ''
+      const module = await buildTestingModule()
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleConfigs).toEqual(expect.objectContaining({ CAPTCHA: null }))
+    })
+
+    it('should set the rules config depending on env variables when valid', async () => {
+      process.env.RULE_CAPTCHA_CONFIG = '{"test": true}'
+      const module = await buildTestingModule()
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleConfigs).toEqual(expect.objectContaining({CAPTCHA: {test: true}}))
+    })
   })
 
-  it('should disable the rules depending on env variables when empty', async () => {
-    process.env.RULE_DAILY_CAP_ENABLED = ''
-    const module = await buildTestingModule()
-    const service = module.get<GatewayService>(GatewayService)
-    await service.onModuleInit()
-    expect((service as any).ruleEnabled).toEqual(expect.objectContaining({ DAILY_CAP: false }))
+  describe("by changing the provider config", () => {
+    const buildTestingModuleForConfig = async (config: RulesConfig): Promise<TestingModule> => {
+      return Test.createTestingModule({
+        imports: [
+          HttpModule,
+          ConfigModule.forRoot({
+            load: [thirdPartyConfig, rulesConfig],
+          })
+        ],
+        providers: [
+          GatewayService,
+          CaptchaService,
+          DeviceCheckService,
+          SafetyNetService,
+        ]
+      }).overrideProvider(rulesConfig.KEY).useValue(config).compile()
+    }
+
+    it('should be defined', async () => {
+      const module = await buildTestingModuleForConfig({enabled: {}, configs: {}})
+      const service = module.get<GatewayService>(GatewayService)
+      expect(service).toBeDefined()
+    })
+
+    it('should disable the rules depending on env variables when empty', async () => {
+      const module = await buildTestingModuleForConfig({
+        configs: {},
+        enabled: {
+          [RuleID.DailyCap]: false
+        }
+      })
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleEnabled).toEqual(expect.objectContaining({ DAILY_CAP: false }))
+    })
+
+    it('should enable the rules depending on env variables when true', async () => {
+      const module = await buildTestingModuleForConfig({
+        configs: {},
+        enabled: {
+          [RuleID.DailyCap]: true
+        }
+      })
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleEnabled).toEqual(expect.objectContaining({DAILY_CAP: true}))
+    })
+
+    it('should set the rules config depending on env variables when empty', async () => {
+      const module = await buildTestingModuleForConfig({
+        enabled: {},
+        configs: {
+          [RuleID.Captcha]: null
+        }
+      })
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleConfigs).toEqual(expect.objectContaining({ CAPTCHA: null }))
+    })
+
+    it('should set the rules config depending on env variables when valid', async () => {
+      const module = await buildTestingModuleForConfig({
+        enabled: {},
+        configs: {
+          [RuleID.Captcha]: {test: true}
+        }
+      })
+      const service = module.get<GatewayService>(GatewayService)
+      await service.onModuleInit()
+      expect((service as any).ruleConfigs).toEqual(expect.objectContaining({CAPTCHA: {test: true}}))
+    })
   })
 
-  it('should enable the rules depending on env variables when true', async () => {
-    process.env.RULE_DAILY_CAP_ENABLED = 'true'
-    const module = await buildTestingModule()
-    const service = module.get<GatewayService>(GatewayService)
-    await service.onModuleInit()
-    expect((service as any).ruleEnabled).toEqual(expect.objectContaining({DAILY_CAP: true}))
-  })
-
-  it('should set the rules config depending on env variables when empty', async () => {
-    process.env.RULE_CAPTCHA_CONFIG = ''
-    const module = await buildTestingModule()
-    const service = module.get<GatewayService>(GatewayService)
-    await service.onModuleInit()
-    expect((service as any).ruleConfigs).toEqual(expect.objectContaining({ CAPTCHA: null }))
-  })
-
-  it('should set the rules config depending on env variables when valid', async () => {
-    process.env.RULE_CAPTCHA_CONFIG = '{"test": true}'
-    const module = await buildTestingModule()
-    const service = module.get<GatewayService>(GatewayService)
-    await service.onModuleInit()
-    expect((service as any).ruleConfigs).toEqual(expect.objectContaining({CAPTCHA: {test: true}}))
-  })
 })
