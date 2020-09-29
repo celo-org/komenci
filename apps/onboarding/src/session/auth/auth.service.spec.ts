d3bfd1eed09486671735403045e3cfd59c499452
Bogdan Dumitru <bogdan@bowd.io>
2020-09-29 13:31:51 +0200

    Simplify auth.service test

--- apps/onboarding/src/session/auth/auth.service.spec.ts
+++ apps/onboarding/src/session/auth/auth.service.spec.ts
@@ -1,35 +1,32 @@
-import { JwtModule, JwtService } from '@nestjs/jwt'
+import { JwtModule } from '@nestjs/jwt'
 import { Test, TestingModule } from '@nestjs/testing'
-import { getRepositoryToken } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
-import{ Session } from '../session.entity'
-import {SessionRepository} from '../session.repository'
 import { SessionService } from '../session.service'
-import { AuthController } from './auth.controller'
-import { AuthModule } from './auth.module'
 import { AuthService } from './auth.service'
-import { JwtStrategy } from './jwt.strategy'
 
 describe('AuthService', () => {
   let service: AuthService
-  let repository: SessionRepository
+  const mockSessionService = {
+    createSession: jest.fn(),
+    findByAccount: jest.fn(),
+    findOne: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule,     
+      imports: [
         JwtModule.register({
-        secret: 'secret12356789',
+          secret: 'secret12356789',
         })
       ],
-      controllers: [AuthController],
-      providers: [SessionService,        {
-        provide: getRepositoryToken(Session),
-        useClass: Repository,
-      },
-      AuthService, JwtStrategy]
+      providers: [
+        {
+          provide: SessionService,
+          useValue: mockSessionService
+        },
+        AuthService,
+      ]
     }).compile()
     
-    repository = module.get<Repository<Session>>(getRepositoryToken(Session))
     service = module.get<AuthService>(AuthService)
   })
 
