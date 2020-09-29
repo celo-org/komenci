b41cb66c4383718a7afc2c7ba2191d8dca0567c4
enrique <enrique@keyko.io>
2020-09-29 12:36:31 +0200

    Add changes in test

--- apps/onboarding/src/app.controller.spec.ts
+++ apps/onboarding/src/app.controller.spec.ts
@@ -1,22 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from './app.controller'
+import { AppModule } from './app.module'
 import { AppService } from './app.service'
+import { GatewayService } from './gateway/gateway.service'
 
 describe('AppController', () => {
   let appController: AppController
+  const gatewayMock = {
+    verify: jest.fn(() => false)
+  }
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
       controllers: [AppController],
       providers: [AppService]
-    }).compile()
+    })
+    .overrideProvider(GatewayService)
+    .useValue(gatewayMock)
+    .compile()
 
     appController = app.get<AppController>(AppController)
   })
 
   xdescribe('root', () => {
     it('should return "Hello World!"', () => {
-      // expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.startSession({} as any,{})).toBe('Hello World!')
     })
   })
 })
