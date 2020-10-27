7388d9913c1e4b20aae400066ae02f70f9ec02d3
Bogdan Dumitru <bogdan@bowd.io>
2020-10-27 12:13:16 +0100

    Use EIP712 for the login signature

--- apps/onboarding/src/gateway/rules/signature.rule.ts
+++ apps/onboarding/src/gateway/rules/signature.rule.ts
@@ -1,7 +1,9 @@
 import { StartSessionDto } from '@app/onboarding/dto/StartSessionDto'
+import { ensureLeading0x } from '@celo/base'
 import { Err, Ok, RootError } from '@celo/base/lib/result'
-import { recoverMessageSigner } from '@celo/contractkit/lib/utils/signing-utils'
-import { hashMessage } from '@celo/utils/lib/signatureUtils'
+import { buildLoginTypedData } from '@celo/komencikit/lib/login'
+import { generateTypedDataHash } from '@celo/utils/lib/sign-typed-data-utils'
+import { parseSignatureWithoutPrefix } from '@celo/utils/lib/signatureUtils'
 import { Injectable } from '@nestjs/common'
 import { Rule, RuleID } from './rule'
 
@@ -10,7 +12,7 @@ export enum SignatureErrorTypes {
 }
 
 export class InvalidSignature extends RootError<SignatureErrorTypes> {
-  constructor() {
+  constructor(public readonly error: Error) {
     super(SignatureErrorTypes.InvalidSignature)
   }
 }
@@ -22,13 +24,23 @@ export class SignatureRule implements Rule<{}, InvalidSignature> {
   }
 
   async verify(startSessionDto: StartSessionDto, config, context) {
+    const loginTypedData = buildLoginTypedData(
+      startSessionDto.externalAccount,
+      startSessionDto.captchaResponseToken
+    )
+    const messageHash = ensureLeading0x(generateTypedDataHash(loginTypedData).toString('hex'))
     const signature = startSessionDto.signature
-    const account = startSessionDto.externalAccount
-    const message = hashMessage(`komenci:login:${account}`)
-    const signer = recoverMessageSigner(message, signature)
+    const signer = startSessionDto.externalAccount
 
-    if (signer.toLocaleLowerCase() !== account.toLocaleLowerCase()) {
-      return Err(new InvalidSignature())
+    try {
+      console.log(messageHash)
+      console.log(signature)
+      console.log(signer)
+      const parsedSig = parseSignatureWithoutPrefix(messageHash, signature, signer)
+      console.log(parsedSig)
+    } catch (e) {
+      console.log(e)
+      return Err(new InvalidSignature(e))
     }
 
     return Ok(true)
