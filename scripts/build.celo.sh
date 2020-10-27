ed2bdd119b2e72047a91b34a651e3ad2b51bc409
Bogdan Dumitru <bogdan@bowd.io>
2020-10-27 14:21:02 +0100

    Change hash key for celo deps

--- scripts/build.celo.sh
+++ scripts/build.celo.sh
@@ -5,4 +5,5 @@ yarn --cwd ./libs/celo/packages/utils build
 yarn --cwd ./libs/celo/packages/dev-utils build
 yarn --cwd ./libs/celo/packages/contractkit build:gen
 yarn --cwd ./libs/celo/packages/contractkit build
+yarn --cwd ./libs/celo/packages/komencikit build
 yarn --cwd ./libs/celo/packages/phone-number-privacy/common build
