6ad20c87e8f4e892946591de11515390e5822b4c
Bogdan Dumitru <bogdan@bowd.io>
2020-10-28 14:34:25 +0100

    Cleanup error clasification

--- apps/onboarding/src/errors/api-error.filter.ts
+++ apps/onboarding/src/errors/api-error.filter.ts
@@ -3,6 +3,7 @@ import { AbstractHttpAdapter, BaseExceptionFilter } from '@nestjs/core'
 import { MESSAGES } from '@nestjs/core/constants'
 import { Logger } from 'nestjs-pino'
 
+import { RootError } from '@celo/base/lib/result'
 import { ApiError } from './api-error'
 
 @Catch()
@@ -16,8 +17,8 @@ export class ApiErrorFilter extends BaseExceptionFilter {
     host: ArgumentsHost,
     applicationRef: AbstractHttpAdapter | HttpServer
   ) {
-    if (ApiError.isApiError(exception)) {
-      const apiError = exception as ApiError<any>
+    if (this.isApiError(exception)) {
+      const apiError = exception as ApiError<any, any>
       const res = apiError.toJSON()
       applicationRef.reply(host.getArgByIndex(1), res, apiError.statusCode)
       return this.logger.error(
@@ -34,13 +35,32 @@ export class ApiErrorFilter extends BaseExceptionFilter {
         message: MESSAGES.UNKNOWN_EXCEPTION_MESSAGE,
       }
       applicationRef.reply(host.getArgByIndex(1), body, body.statusCode)
-      if (this.isExceptionObject(exception)) {
+      if (this.isRootError(exception)) {
+        const rootError = exception as RootError<any>
+        return this.logger.error(
+          {
+            errorType: rootError.errorType,
+            message: rootError.message,
+          },
+          rootError.stack,
+        )
+      } else if (this.isExceptionObject(exception)) {
         return this.logger.error(
           exception.message,
           exception.stack,
         )
+      } else {
+        return this.logger.error(exception)
       }
-      return this.logger.error(exception)
     }
+  }
+
+  isRootError(exception: any) {
+    return exception.errorType !== undefined
+  }
+
+  isApiError(exception: any) {
+    return exception._apiError === true
+      && typeof(exception.toJSON) === 'function'
   }
 }
--- apps/onboarding/src/errors/api-error.ts
+++ apps/onboarding/src/errors/api-error.ts
@@ -1,12 +1,6 @@
 import { RootError } from '@celo/base/lib/result'
 
 export abstract class ApiError<TError, TMetadata = never> extends RootError<TError> {
-  // Can not use instanceof with classes that extend Error
-  static isApiError(obj: any): boolean {
-    return obj._apiError === true
-      && typeof(obj.toJSON) === 'function'
-  }
-
   _apiError: boolean = true
   abstract statusCode: number
   metadata?: TMetadata
