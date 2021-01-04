15d2e80f2544388659b8434aab34ca17df5e5073
Enrique Ruiz <enrique@keyko.io>
2021-01-04 12:41:12 +0100

    Eruizgar91/add typeorm migrations (#144)
    * Adding script to create entity schema
    
    * Udpate config
    
    * Document the migration process
    
    * Update README.md
    
    Co-authored-by: Cody Born <codyborn@outlook.com>
    
    * Migrations when starting
    
    * Fix linting
    
    * Update README.md
    
    Co-authored-by: Valentin Rodygin <valentin@clabs.co>
    
    * Update apps/onboarding/src/migrations/1607954006850-session.ts
    
    Co-authored-by: Bogdan Dumitru <304771+bowd@users.noreply.github.com>
    
    Co-authored-by: Cody Born <codyborn@outlook.com>
    Co-authored-by: Valentin Rodygin <valentin@clabs.co>
    Co-authored-by: Bogdan Dumitru <304771+bowd@users.noreply.github.com>

---
+++ apps/onboarding/src/migrations/1607954006850-session.ts
@@ -0,0 +1,49 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm"
+
+/* tslint:disable */
+export class session1607954006850 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.createTable(
+          new Table({
+            name: 'session',
+            columns: [
+              {
+                name: 'id',
+                type: 'uuid',
+                // isPrimary: true,
+              },
+              {
+                name: 'externalAccount',
+                type: 'varchar',
+                isNullable: false,
+              },
+              {
+                name: 'meta',
+                type: 'json',
+                isNullable: true
+              },
+              {
+                name: 'createdAt',
+                type: 'timestamp',
+                isNullable: false
+              },
+              {
+                name: 'expiredAt',
+                type: 'timestamp',
+                isNullable: true
+              },
+              {
+                name: 'completedAt',
+                type: 'timestamp',
+                isNullable: true
+              }
+            ]
+          }),
+          true,
+        )
+      }
+      public async down(queryRunner: QueryRunner): Promise<any> {
+        queryRunner.query(`DROP TABLE session`)
+      }
+}
