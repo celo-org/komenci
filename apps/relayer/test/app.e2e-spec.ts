af70612820011a06db12bb894396a348971aab5e
enrique <enrique@keyko.io>
2021-01-25 14:07:18 +0100

    Add last progress

--- apps/relayer/test/app.e2e-spec.ts
+++ apps/relayer/test/app.e2e-spec.ts
@@ -10,14 +10,19 @@ import { BlockchainModule, ContractsModule } from '@app/blockchain'
 import * as metaTxWalletDeployer from '../../../libs/celo/packages/protocol/build/contracts/MetaTransactionWalletDeployer.json'
 import * as metaTxWallet from '../../../libs/celo/packages/protocol/build/contracts/MetaTransactionWallet.json'
 import { AppController, RelayerCmd } from '../src/app.controller'
-import { walletConfig, WalletType } from '@app/blockchain/config/wallet.config'
+import { WalletConfig, walletConfig, WalletType } from '@app/blockchain/config/wallet.config'
 import { SignPersonalMessageDto } from '../src/dto/SignPersonalMessageDto'
 import { KomenciLoggerModule, KomenciLoggerService } from '@app/komenci-logger'
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { appConfig } from '@app/onboarding/config/app.config'
+import { AppConfig, appConfig } from '../src/config/app.config'
 import { NodeConfig, nodeConfig } from '@app/blockchain/config/node.config'
 import { OdisService } from '../src/odis/odis.service'
 import { ACCOUNT_ADDRESS, PRIVATE_KEY } from '../src/config/testing-constants'
+import { MetaTransactionWalletWrapper } from '@celo/contractkit/lib/wrappers/MetaTransactionWallet'
+import { TransactionService } from '../src/chain/transaction.service'
+import { BalanceService } from '../src/chain/balance.service'
+import { BlockchainService } from '@app/blockchain/blockchain.service'
+import { AppModule } from '../src/app.module'
 
 
 describe('relayer', () => {
@@ -34,7 +39,7 @@ describe('relayer', () => {
 
   networkCfg = {
     relayers: [ {
-      externalAccount: "",
+      externalAccount: "0xa7d74cb4fca9458757cfc8b90d9b38a126f68b47",
       metaTransactionWallet: ""
     }
     ],
@@ -42,60 +47,76 @@ describe('relayer', () => {
       MetaTransactionWalletDeployer: metaTxWalletDeployer.networks[1101].address,
       MetaTransactionWalletVersions: metaTxWalletVersion
     },
-    fornoURL: "",
+    fornoURL: "'http://localhost:8545",
     odis: {
       publicKey: "",
       url: ""
     }
   }
 
-
-  
-
   beforeAll(async () => {
     web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-
-    const accounts = await web3.eth.getAccounts()
-    networkCfg.relayers[0].externalAccount= accounts[0]
-    networkCfg.relayers[0].metaTransactionWallet= accounts[1]
-
+    
     const mockWallet: LocalWallet = new LocalWallet()
     mockWallet.addAccount(PRIVATE_KEY)
 
-    kit = newKitFromWeb3(web3, mockWallet)
+
+    const accounts = await web3.eth.getAccounts()
+    networkCfg.relayers[0].metaTransactionWallet= metaTxWallet.networks[1101].address
+    // networkCfg.relayers[0].externalAccount= accounts[0]
+
+
+    kit = new ContractKit(
+      new Web3(new Web3.providers.HttpProvider(process.env.NODE_URL)),
+      mockWallet
+    )
+    const appConfigValue: Partial<AppConfig> = {
+      maxGasPrice: "3000000000",
+      gasPriceMultiplier: 5,
+      gasPriceFallback: "1000000000",
+    }
+
+    const walletDeployer = new web3.eth.Contract(metaTxWalletDeployer.abi, metaTxWalletDeployer.networks[1101].address)
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
-        ConfigModule.forRoot({
-          isGlobal: true,
-          load: [appConfig, walletConfig, nodeConfig],
-          envFilePath: ['apps/relayer/.env.local']
-        }),
-        BlockchainModule.forRootAsync({
-          inject: [ConfigService],
-          useFactory: (config: ConfigService) => {
-            return {
-              node: config.get<NodeConfig>('node'),
-              wallet: {
-                type: WalletType.Local,
-                address: ACCOUNT_ADDRESS,
-                privateKey: PRIVATE_KEY
-              },
-            }
-          }
-        }),        ContractsModule,
-        ContractsModule,
+        AppModule,
+        // ConfigModule.forRoot({
+        //   isGlobal: true,
+        //   load: [appConfig, walletConfig, nodeConfig],
+        //   envFilePath: ['apps/relayer/.env.local']
+        // }),
+        // BlockchainModule.forRootAsync({
+        //   inject: [ConfigService],
+        //   useFactory: (config: ConfigService) => {
+        //     return {
+        //       node: config.get<NodeConfig>('node'),
+        //       wallet: {
+        //         type: WalletType.Local,
+        //         address: ACCOUNT_ADDRESS,
+        //         privateKey: PRIVATE_KEY
+        //       },
+        //     }
+        //   }
+        // }),        
+        // ContractsModule,
         ClientsModule.register([
           { name: 'RELAYER_SERVICE', transport: Transport.TCP },
         ]),
-        KomenciLoggerModule.forRoot({
-          pinoHttp: {
-            prettyPrint: true
-          }
-        })
+        // KomenciLoggerModule.forRoot({
+        //   pinoHttp: {
+        //     prettyPrint: true
+        //   }
+        // })
       ],
       // controllers: [AppController],
-      // providers: [OdisService,
+      // providers: [
+      //   OdisService,
       //   ConfigService,
+      //   { 
+      //     provide: networkConfig.KEY, 
+      //     useValue: networkCfg 
+      //   },
       //   {
       //     provide: WALLET,
       //     useValue: mockWallet
@@ -104,10 +125,18 @@ describe('relayer', () => {
       //     provide: ContractKit,
       //     useValue: kit
       //   },
-      //   KomenciLoggerService]
+      //   {
+      //     provide: MetaTransactionWalletWrapper,
+      //     useValue: walletDeployer
+      //   },
+      //   TransactionService,
+      //   BalanceService,
+      //   KomenciLoggerService
+      // ]
     })
     .overrideProvider(networkConfig.KEY).useValue(networkCfg)
     .overrideProvider(walletConfig).useValue(mockWallet)
+    .overrideProvider(appConfig).useValue(appConfigValue)
     .overrideProvider(WEB3_PROVIDER).useValue('http://localhost:8545').compile()
 
 
@@ -120,7 +149,7 @@ describe('relayer', () => {
     await app.init()
 
     client = app.get('RELAYER_SERVICE')
-    // controller = moduleFixture.get<AppController>(AppController)
+    controller = moduleFixture.get<AppController>(AppController)
 
     await client.connect()
   })
@@ -138,54 +167,29 @@ describe('relayer', () => {
       expect(app).toBeDefined()
 
 
-      // const s: SignPersonalMessageDto = { data: 'test' }
-      // controller.signPersonalMessage(s)
-
+      const s: SignPersonalMessageDto = { data: 'test' }
+      console.log(await controller.signPersonalMessage(s))
 
       // Call method without wrappers
-      const walletDeployer = new web3.eth.Contract(metaTxWalletDeployer.abi, metaTxWalletDeployer.networks[1101].address)
-      const wallet = new web3.eth.Contract(metaTxWallet.abi, metaTxWallet.networks[1101].address)
-      const method = walletDeployer.methods["deploy"]
-      const methodInstance = method(networkCfg.relayers[0].externalAccount,
-        metaTxWallet.networks[1101].address,
-        wallet.methods.initialize(networkCfg.relayers[0].externalAccount).encodeABI())
+      // const walletDeployer = new web3.eth.Contract(metaTxWalletDeployer.abi, metaTxWalletDeployer.networks[1101].address)
+      // const wallet = new web3.eth.Contract(metaTxWallet.abi, metaTxWallet.networks[1101].address)
+      // const method = walletDeployer.methods["deploy"]
+      // const methodInstance = method(networkCfg.relayers[0].externalAccount,
+      //   metaTxWallet.networks[1101].address,
+      //   wallet.methods.initialize(networkCfg.relayers[0].externalAccount).encodeABI())
 
-      const from = networkCfg.relayers[0].externalAccount
-      const gas = await methodInstance.estimateGas(networkCfg.relayers[0].externalAccount,
-        metaTxWallet.networks[1101].address,
-        wallet.methods.initialize(networkCfg.relayers[0].externalAccount).encodeABI(), {
-          from
-      })
-      const tx = await methodInstance.send({
-          from,
-          gas
-      })
-      console.log(tx)
+      // const from = networkCfg.relayers[0].externalAccount
+      // const gas = await methodInstance.estimateGas(networkCfg.relayers[0].externalAccount,
+      //   metaTxWallet.networks[1101].address,
+      //   wallet.methods.initialize(networkCfg.relayers[0].externalAccount).encodeABI(), {
+      //     from
+      // })
+      // const tx = await methodInstance.send({
+      //     from,
+      //     gas
+      // })
+      // console.log(tx)
 
-
-
-
-
-
-      // const init = await wallet.methods.initialize.send(accounts[0], accounts[1])
-      // console.log(init)
-      
-      // console.log(await walletDeployer.methods.deploy.send(accounts[0], accounts[0], metaTxWallet.networks[1101].address, init))
-      // console.log(await walletDeployer.methods.getVersionNumber().call())
-
-      // this.walletDeployer.deploy(
-      //   session.externalAccount,
-      //   implementationAddress,
-      //   impl.methods.initialize(session.externalAccount).encodeABI()
-      // ).sendAndWaitForReceipt()
-
-      // const deployer = app.get(MetaTransactionWalletDeployerWrapper)
-      // console.log(deployer)
-      
-      
-
-      // const sign = client.send({ cmd:  RelayerCmd.SignPersonalMessage },JSON.stringify({ data: 'test' }))
-      // sign.toPromise().catch((err) => console.log('caught it'))
 
     })
   })
