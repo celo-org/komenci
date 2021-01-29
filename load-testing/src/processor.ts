dd24777bf561fadd0a897df464b76e3a7f5fef23
enrique <enrique@keyko.io>
2021-01-29 14:34:21 +0100

    Working until the second check session

--- load-testing/src/processor.ts
+++ load-testing/src/processor.ts
@@ -1,29 +1,220 @@
-import { query } from 'express';
 import fs = require('fs')
 import path = require('path')
-import { loadInputData } from './utils'
+import { getLoginSignature, getAddressFromDeploy } from './utils'
+const Web3 = require('web3')
+const { buildLoginTypedData } =  require('../../libs/celo/packages/komencikit/lib/login')
+const { compressedPubKey } = require('../../libs/celo/packages/utils/lib/dataEncryptionKey')
+const { base64ToHex, hexToBuffer } = require('../../libs/celo/packages/base')
+const { LocalWallet } = require('../../libs/celo/packages/contractkit/lib/wallets/local-wallet')
+const { newKitFromWeb3 } = require('../../libs/celo/packages/contractkit/lib/kit')
+const { serializeSignature } = require('../../libs/celo/packages/base/lib/signatureUtils')
+const { normalizeAddressWith0x } = require('../../libs/celo/packages/base/lib/address')
+const { Err, Ok } = require('../../libs/celo/packages/base/lib/result')
+const { InvalidWallet } = require('../../libs/celo/packages/komencikit/lib/errors')
+const { verifyWallet } = require('../../libs/celo/packages/komencikit/lib/verifyWallet')
+const { WasmBlsBlindingClient } = require('../../libs/celo/packages/contractkit/lib/identity/odis/bls-blinding-client')
+const { getBlindedPhoneNumber, getPhoneNumberIdentifierFromSignature } = require('../../libs/celo/packages/contractkit/lib/identity/odis/phone-number-identifier')
+const { toRawTransaction } = require('../../libs/celo/packages/contractkit/lib/wrappers/MetaTransactionWallet')
+
 
 let args = {};
-
-function loadTestCases(context, events, done) {
-  const environment = context.vars.$environment
-
-  if (!args[environment]) {
-    console.log(`Loading test cases for environment: ${environment}`)
-
-    const __rootpath = path.join(__dirname, '/../')
-    const __inputpath = path.join(__rootpath, '/input/', environment)
-
-    // Configuration of ADDRESS test case
-    const inputAddresses = loadInputData(path.join(__inputpath, 'addresses.csv'))
-    const celoAccounts = loadInputData(path.join(__inputpath, 'celo_accounts.csv'))
-    const celoValidators = loadInputData(path.join(__inputpath, 'celo_validators.csv'))
-
-
-    args[environment] = inputAddresses
-
-  }
-  return args[environment]
+enum Network {
+  alfajores = 'alfajores',
+  rc1 = 'rc1',
+}
+const wallet = new LocalWallet()
+const fornoURL: Record<Network, string> = {
+  alfajores: 'https://alfajores-forno.celo-testnet.org',
+  rc1: 'https://rc1-forno.celo-testnet.org',
 }
 
-module.exports = { loadTestCases }
+
+const WALLET_IMPLEMENTATIONS: Record<Network, Record<string, string>> = {
+  [Network.alfajores]: {
+    '1.1.0.0-p1': '0x88a2b9B8387A1823D821E406b4e951337fa1D46D',
+    '1.1.0.0-p2': '0x786ec5A4F8DCad3A58D5B1A04cc99B019E426065',
+    '1.1.0.0-p3': '0x5C9a6E3c3E862eD306E2E3348EBC8b8310A99e5A',
+  },
+  [Network.rc1]: {
+    '1.1.0.0-p2': '0x6511FB5DBfe95859d8759AdAd5503D656E2555d7',
+  },
+}
+
+const ODIS_PUB_KEYS: Record<Network, string> = {
+  alfajores:
+    'kPoRxWdEdZ/Nd3uQnp3FJFs54zuiS+ksqvOm9x8vY6KHPG8jrfqysvIRU0wtqYsBKA7SoAsICMBv8C/Fb2ZpDOqhSqvr/sZbZoHmQfvbqrzbtDIPvUIrHgRS0ydJCMsA',
+  rc1:
+    'FvreHfLmhBjwxHxsxeyrcOLtSonC9j7K3WrS4QapYsQH6LdaDTaNGmnlQMfFY04Bp/K4wAvqQwO9/bqPVCKf8Ze8OZo8Frmog4JY4xAiwrsqOXxug11+htjEe1pj4uMA',
+}
+
+const provider = new Web3.providers.HttpProvider(fornoURL[Network.alfajores])
+const web3 = new Web3(provider)
+const contractKit = newKitFromWeb3(web3, wallet)
+let environment
+const captchaToken = 'special-captcha-bypass-token'
+const pkey = Web3.utils.randomHex(32)
+const dek = Web3.utils.randomHex(32)
+wallet.addAccount(pkey)
+const account = wallet.getAccounts()[0]
+let externalAccount = normalizeAddressWith0x(account)
+const dekPublicKey = compressedPubKey(hexToBuffer(dek))
+
+let metaTxWalletAddress
+
+
+const e164Number = "+40723301264"
+const blsBlindingClient = new WasmBlsBlindingClient("kPoRxWdEdZ/Nd3uQnp3FJFs54zuiS+ksqvOm9x8vY6KHPG8jrfqysvIRU0wtqYsBKA7SoAsICMBv8C/Fb2ZpDOqhSqvr/sZbZoHmQfvbqrzbtDIPvUIrHgRS0ydJCMsA")
+
+async function setStartSessionBody(requestParams, context, ee, next) {
+  environment = context.vars.$environment
+  console.log(`Loading test cases for environment: ${environment}`)
+  // console.log(context.vars.$loopCount)
+
+  const loginStruct = buildLoginTypedData(externalAccount, captchaToken)
+  const signature = await contractKit.signTypedData(
+    externalAccount,
+    loginStruct
+  )
+  const serializedSignature = serializeSignature(signature)
+  // const serializedSignature = await getLoginSignature(account, captchaToken)
+  // if (!serializedSignature.ok) {
+  //   return serializedSignature
+  // }
+
+
+  requestParams.json = {
+    externalAccount: externalAccount,
+    captchaResponseToken: captchaToken,
+    signature: serializedSignature
+  }
+  return next(); // MUST be called for the scenario to continue
+}
+
+async function setDeployWalletBody(requestParams, context, ee, next) {
+  // const walletImplementationAddress = WALLET_IMPLEMENTATIONS[environment]
+  // console.log(walletImplementationAddress)
+  requestParams.json = {
+    implementationAddress: "0x5C9a6E3c3E862eD306E2E3348EBC8b8310A99e5A" //walletImplementationAddress
+  }
+  return next() 
+}
+
+async function setDistributedBlindedPeppertBody(requestParams, context, ee, next) {
+  // const walletImplementationAddress = WALLET_IMPLEMENTATIONS[environment]
+  // console.log(walletImplementationAddress)
+  const blindedPhoneNumber = await getBlindedPhoneNumber(e164Number, blsBlindingClient)
+
+  requestParams.json = {
+    blindedPhoneNumber: blindedPhoneNumber, 
+    clientVersion: "1.1.0.0-p3"
+  }
+  return next() 
+}
+
+async function setRequestSubsidisedAttestationsBody(requestParams, context, ee, next) {
+
+  // const setAccount = await komenciKit.setAccount(walletAddress, '', dekPublicKey, account)
+
+  // const accounts = await this.contractKit.contracts.getAccounts()
+  // const proofOfPossession = await accounts.generateProofOfKeyPossession(
+  //   metaTxWalletAddress,
+  //   walletAddress
+  // )
+  // const attestations = await this.contractKit.contracts.getAttestations()
+  // const wallet = await this.getWallet(metaTxWalletAddress)
+  // const nonce = await wallet.nonce()
+
+  // const approveTx = await attestations.approveAttestationFee(attestationsRequested)
+  // const approveTxSig = await wallet.signMetaTransaction(approveTx.txo, nonce)
+  // const approveMetaTx = wallet.executeMetaTransaction(approveTx.txo, approveTxSig)
+
+  // const requestTx = await attestations.request(identifier, attestationsRequested)
+  // const requestTxSig = await wallet.signMetaTransaction(requestTx.txo, nonce + 1)
+  // const requestMetaTx = wallet.executeMetaTransaction(requestTx.txo, requestTxSig)
+
+  // requestParams.json = {
+  //   identifier: identifier,
+  //   attestationsRequested: attestationsRequested,
+  //   walletAddress: metaTxWalletAddress,
+  //   requestTx: toRawTransaction(requestMetaTx.txo),
+  //   approveTx: toRawTransaction(approveMetaTx.txo),
+  // }
+  return next() 
+}
+
+async function afterDistributedBlindedPepper(requestParams, response, context, ee, next) {
+  const phoneNumberHashDetails = await getPhoneNumberIdentifierFromSignature(
+    e164Number,
+    response.body.combinedSignature,
+    blsBlindingClient
+  )
+  // Need to pass to the attestation
+  console.log({
+    identifier: phoneNumberHashDetails.phoneHash,
+    pepper: phoneNumberHashDetails.pepper,
+  })
+  return next()
+}
+
+// For setAccount
+async function setSubmitMetatransactionBody(requestParams, context, ee, next){
+
+  const accounts = await contractKit.contracts.getAccounts()
+  console.log("Meta", metaTxWalletAddress)
+  console.log("External", externalAccount)
+  const proofOfPossession = await accounts.generateProofOfKeyPossession(
+    metaTxWalletAddress,
+    externalAccount
+  )
+  const tx = accounts.setAccount('', dekPublicKey, externalAccount, proofOfPossession)
+
+  const wallet = await getWallet(metaTxWalletAddress)
+  const signature = await wallet.signMetaTransaction(tx.txo)//, nonce)
+  const rawMetaTx = toRawTransaction(wallet.executeMetaTransaction(tx.txo, signature).txo)
+
+  console.log("XXXXX")
+  console.log(rawMetaTx)
+  requestParams.json = {
+    rawMetaTx: rawMetaTx
+  }
+}
+
+async function waitTx(requestParams, response, context, ee, next) {
+
+  metaTxWalletAddress =
+  response.body.status === 'deployed'
+      ? Ok(response.body.walletAddress)
+      : await getAddressFromDeploy(response.body.txHash)
+
+  if (!metaTxWalletAddress.ok) {
+    return metaTxWalletAddress
+  }
+
+  const walletStatus = await verifyWallet(
+    contractKit,
+    metaTxWalletAddress.result,
+    ['0x5C9a6E3c3E862eD306E2E3348EBC8b8310A99e5A'], // pending to get from variable
+    externalAccount
+  )
+
+  if (!walletStatus.ok) {
+    return Err(new InvalidWallet(walletStatus.error))
+  }
+  return next() // MUST be called for the scenario to continue
+}
+
+function logHeaders(requestParams, response, context, ee, next) {
+  console.log(response.body)
+  return next() // MUST be called for the scenario to continue
+}
+
+
+let _wallet
+async function getWallet(address: string){
+  if (_wallet?.address !== address) {
+    _wallet = await contractKit.contracts.getMetaTransactionWallet(address)
+  }
+  return _wallet
+}
+
+module.exports = { logHeaders, setStartSessionBody, setDeployWalletBody, setDistributedBlindedPeppertBody , waitTx, afterDistributedBlindedPepper, setRequestSubsidisedAttestationsBody, setSubmitMetatransactionBody}
--- load-testing/src/utils.ts
+++ load-testing/src/utils.ts
@@ -1,14 +1,168 @@
-const fs = require('fs')
-const parse = require('csv-parse/lib/sync')
+const {serializeSignature} = require('../../libs/celo/packages/base/lib/signatureUtils')
+import { buildLoginTypedData } from '../../libs/celo/packages/komencikit/lib/login'
+const {Err, Ok} = require('../../libs/celo/packages/base/lib/result')
+import { EventLog } from 'web3-core';
+const {normalizeAddressWith0x} = require('../../libs/celo/packages/base/lib/address')
+const {sleep} = require('../../libs/celo/packages/utils/lib/async')
+const { TxEventNotFound, TxTimeoutError, TxRevertError, LoginSignatureError } = require('../../libs/celo/packages/komencikit/lib/errors')
+const Web3 = require('web3')
+const { LocalWallet } = require('../../libs/celo/packages/contractkit/lib/wallets/local-wallet')
+const newKitFromWeb3 = require('../../libs/celo/packages/contractkit/lib/kit').newKitFromWeb3
 
-export function loadInputData(filePath: string): Array<string> {
-
-    const fileContents = fs.readFileSync(filePath,'utf8')
-
-    const contents = parse(fileContents, {delimiter: ',', columns: false, skip_empty_lines: true})
-    let output = []
-    contents.map( (record) => {
-        output.push({param0: record[0]})
-    })
-    return output
+interface ContractEventLog<T> extends EventLog {
+  returnValues: T;
 }
+
+
+let args = {};
+enum Network {
+  alfajores = 'alfajores',
+  rc1 = 'rc1',
+}
+const wallet = new LocalWallet()
+const fornoURL: Record<Network, string> = {
+  alfajores: 'https://alfajores-forno.celo-testnet.org',
+  rc1: 'https://rc1-forno.celo-testnet.org',
+}
+
+
+const WALLET_IMPLEMENTATIONS: Record<Network, Record<string, string>> = {
+  [Network.alfajores]: {
+    '1.1.0.0-p1': '0x88a2b9B8387A1823D821E406b4e951337fa1D46D',
+    '1.1.0.0-p2': '0x786ec5A4F8DCad3A58D5B1A04cc99B019E426065',
+    '1.1.0.0-p3': '0x5C9a6E3c3E862eD306E2E3348EBC8b8310A99e5A',
+  },
+  [Network.rc1]: {
+    '1.1.0.0-p2': '0x6511FB5DBfe95859d8759AdAd5503D656E2555d7',
+  },
+}
+
+const ODIS_PUB_KEYS: Record<Network, string> = {
+  alfajores:
+    'kPoRxWdEdZ/Nd3uQnp3FJFs54zuiS+ksqvOm9x8vY6KHPG8jrfqysvIRU0wtqYsBKA7SoAsICMBv8C/Fb2ZpDOqhSqvr/sZbZoHmQfvbqrzbtDIPvUIrHgRS0ydJCMsA',
+  rc1:
+    'FvreHfLmhBjwxHxsxeyrcOLtSonC9j7K3WrS4QapYsQH6LdaDTaNGmnlQMfFY04Bp/K4wAvqQwO9/bqPVCKf8Ze8OZo8Frmog4JY4xAiwrsqOXxug11+htjEe1pj4uMA',
+}
+
+const provider = new Web3.providers.HttpProvider(fornoURL[Network.alfajores])
+const web3 = new Web3(provider)
+const contractKit = newKitFromWeb3(web3, wallet)
+
+
+    export async function getLoginSignature(
+      account: any,
+      captchaToken: string
+    ){
+      try {
+        const loginStruct = buildLoginTypedData(normalizeAddressWith0x(account), captchaToken)
+        const signature = await contractKit.signTypedData(
+          account,
+          loginStruct
+        )
+
+        return Ok(serializeSignature(signature))
+      } catch (e) {
+        return Err(new LoginSignatureError(e))
+      }
+    }
+
+    /**
+   * Wait for the deploy tx and extract the wallet from events
+   * @param txHash the transaction hash of the wallet deploy tx
+   * @private
+   */
+  export async function getAddressFromDeploy(txHash: string){
+    const receiptResult = await this.waitForReceipt(txHash)
+    if (!receiptResult.ok) {
+      return receiptResult
+    }
+    const receipt = receiptResult.result
+
+    const deployer = await contractKit.contracts.getMetaTransactionWalletDeployer(receipt.to)
+
+    const events = await deployer.getPastEvents(deployer.eventTypes.WalletDeployed, {
+      fromBlock: receipt.blockNumber,
+      toBlock: receipt.blockNumber,
+    })
+
+    const deployWalletLog = events.find(
+      (
+        event: ContractEventLog<{
+          owner: string
+          wallet: string
+          implementation: string
+          0: string
+          1: string
+          2: string
+        }>
+      ) => normalizeAddressWith0x(event.returnValues.owner) === this.externalAccount
+    )
+
+    if (deployWalletLog === undefined) {
+      return Err(new TxEventNotFound(txHash, deployer.eventTypes.WalletDeployed))
+    }
+
+    return Ok(deployWalletLog.returnValues.wallet)
+  }
+
+  export async function waitForReceipt(txHash: string) {
+    let receipt: any | null = null
+    let waited = 0
+    while (receipt == null && waited < 20000) {
+      receipt = await contractKit.web3.eth.getTransactionReceipt(txHash)
+      if (receipt == null) {
+        await sleep(100)
+        waited += 100
+      }
+    }
+
+    if (receipt == null) {
+      return Err(new TxTimeoutError())
+    }
+
+    if (!receipt.status) {
+      // TODO: Possible to extract reason?
+      return Err(new TxRevertError(txHash, ''))
+    }
+
+    return Ok(receipt)
+  }
+
+
+  export async function setAccount(
+    metaTxWalletAddress: string,
+    name: string,
+    dataEncryptionKey: string,
+    walletAddress: any
+  ){
+    const accounts = await contractKit.contracts.getAccounts()
+    const proofOfPossession = await accounts.generateProofOfKeyPossession(
+      metaTxWalletAddress,
+      walletAddress
+    )
+
+    return this.submitMetaTransaction(
+      metaTxWalletAddress,
+      accounts.setAccount(name, dataEncryptionKey, walletAddress, proofOfPossession)
+    )
+  }
+
+
+  // export async function submitMetaTransaction(
+  //   metaTxWalletAddress: string,
+  //   tx: CeloTransactionObject<any>,
+  //   nonce?: number
+  // ){
+  //   const wallet = await this.getWallet(metaTxWalletAddress)
+  //   const signature = await wallet.signMetaTransaction(tx.txo, nonce)
+  //   const rawMetaTx = toRawTransaction(wallet.executeMetaTransaction(tx.txo, signature).txo)
+
+  //   const resp = await this.client.exec(submitMetaTransaction(rawMetaTx))
+  //   if (!resp.ok) {
+  //     return resp
+  //   }
+
+  //   const txHash = resp.result.txHash
+  //   console.debug(`${TAG}/submitMetaTransaction Waiting for transaction receipt: ${txHash}`)
+  //   return this.waitForReceipt(txHash)
+  // }
