93abd530481a862cc71fa6876f9bc7bb6f8ad469
bowd <bogdan@bowd.io>
2021-03-26 11:09:37 +0100

    Remove deprecate scripts and cleanup CI

--- scripts/build.docker.local.sh
+++ scripts/build.docker.local.sh
@@ -3,15 +3,7 @@
 celo_monorepo_hash=$(git submodule status | awk '{print $1}')
 komenci_hash=$(git rev-parse HEAD)
 
-echo "Building komenci for:"
-echo "   celo-monorepo @ ${celo_monorepo_hash}"
-echo "   komenci @ ${komenci_hash}"
-
-if docker image list | grep celo-monorepo-build-base | grep $celo_monorepo_hash; then
-  echo "Skipping build: celo-monorepo image found!"
-else
-  docker build -f Dockerfile.monorepo . -t celo-monorepo-build-base:$celo_monorepo_hash
-fi
+echo "Building komenci version: ${komenci_hash}"
 
 if docker image list | grep komenci | grep $komenci_hash; then
   echo "Skipping build: komenci image found!"
@@ -19,7 +11,6 @@ else
   docker build .\
       -t komenci:$komenci_hash \
       -t celotestnet.azurecr.io/komenci/komenci:$komenci_hash \
-      --build-arg MONOREPO_BUILD_VERSION=$celo_monorepo_hash \
       --build-arg KOMENCI_VERSION=$komenci_hash
 fi
 
