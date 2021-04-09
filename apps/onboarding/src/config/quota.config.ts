915590e45cda5f727c12dd795477d1b7d2b63aec
Bogdan Dumitru <304771+bowd@users.noreply.github.com>
2021-04-09 09:16:00 +0200

    Add rate limit to startSession and a `ready` endpoint (#168)
    * Add a ready endpoint which is ratelimited
    * Add a generous throttle to startSession
    * Update throttler logic to allow rate-limiting
    * Run load tests with rate-limiting in place

--- apps/onboarding/src/config/quota.config.ts
+++ apps/onboarding/src/config/quota.config.ts
@@ -1,4 +1,5 @@
 import { ConfigType, registerAs } from '@nestjs/config'
+import { numberFromEnv } from './utils'
 
 export enum TrackedAction {
   DistributedBlindedPepper = 'distributedBlindedPepper',
@@ -8,23 +9,13 @@ export enum TrackedAction {
 
 export type ActionCounts = {[action in TrackedAction]?: number}
 
-const parseNumber = (
-  key: string,
-  defaultValue: number
-) => {
-  const rawValue = process.env[key]
-  return rawValue && !isNaN(+rawValue)
-    ? +rawValue
-    : defaultValue
-}
-
 export const quotaConfig = registerAs<() => ActionCounts>('quota', () => ({
   [TrackedAction.DistributedBlindedPepper]:
-    parseNumber('QUOTA_DISTRIBUTED_BLINDED_PEPPER', 1),
+    numberFromEnv('QUOTA_DISTRIBUTED_BLINDED_PEPPER', 1),
   [TrackedAction.RequestSubsidisedAttestation]:
-    parseNumber('QUOTA_REQUEST_SUBSIDISED_ATTESTATION', 10),
+    numberFromEnv('QUOTA_REQUEST_SUBSIDISED_ATTESTATION', 10),
   [TrackedAction.SubmitMetaTransaction]:
-    parseNumber('QUOTA_SUBMIT_META_TRANSACTION', 20),
+    numberFromEnv('QUOTA_SUBMIT_META_TRANSACTION', 20),
 }))
 
 export type QuotaConfig = ConfigType<typeof quotaConfig>
