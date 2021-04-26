98388183a0e699810f6c81b6da5bc5b7a31338ae
Gonzalo Nardini <gnardini@clabs.co>
2021-04-26 01:21:11 -0300

    Add index on inviteRewards

--- apps/rewards/src/migrations/1616435724783-CreateInviteReward.ts
+++ apps/rewards/src/migrations/1616435724783-CreateInviteReward.ts
@@ -44,7 +44,8 @@ export class CreateInviteReward1616435724783 implements MigrationInterface {
             type: 'timestamp',
             isNullable: false
           }
-        ]
+        ],
+        indices: [{ columnNames: ['inviter'] }]
       }),
       true
     )
