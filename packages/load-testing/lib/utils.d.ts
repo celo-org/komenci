3d728e0553354689b31382cc5df3cfaf3f8e32af
bowd <bogdan@bowd.io>
2021-05-06 10:13:06 +0200

    Remove load-testing build dir

--- packages/load-testing/lib/utils.d.ts
+++
@@ -1,6 +0,0 @@
-import { ContractKit } from '@celo/contractkit';
-import { LoginSignatureError, TxEventNotFound, TxRevertError, TxTimeoutError } from '@komenci/kit/lib/errors';
-import { TransactionReceipt } from 'web3-core';
-export declare function getLoginSignature(contractKit: ContractKit, account: any, captchaToken: string): Promise<import("@celo/base/lib/result").OkResult<string> | import("@celo/base/lib/result").ErrorResult<LoginSignatureError>>;
-export declare function getAddressFromDeploy(contractKit: any, externalAccount: any, txHash: string): Promise<import("@celo/base/lib/result").ErrorResult<TxTimeoutError> | import("@celo/base/lib/result").ErrorResult<TxRevertError> | import("@celo/base/lib/result").ErrorResult<TxEventNotFound> | import("@celo/base/lib/result").OkResult<any>>;
-export declare function waitForReceipt(contractKit: ContractKit, txHash: string): Promise<import("@celo/base/lib/result").ErrorResult<TxTimeoutError> | import("@celo/base/lib/result").ErrorResult<TxRevertError> | import("@celo/base/lib/result").OkResult<TransactionReceipt>>;
