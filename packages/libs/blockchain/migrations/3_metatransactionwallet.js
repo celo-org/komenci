2e073f8adbfa30829d71e23a42a6012c2c6722ea
bowd <bogdan@bowd.io>
2021-05-11 14:33:09 +0200

    Initialize MTW implementation correctly

--- packages/libs/blockchain/migrations/3_metatransactionwallet.js
+++ packages/libs/blockchain/migrations/3_metatransactionwallet.js
@@ -3,5 +3,5 @@ const { NULL_ADDRESS } = require("@celo/base/lib/address");
 const MetaTransactionWallet = artifacts.require("MetaTransactionWallet");
 
 module.exports = async function (deployer, network, addresses) {
-  await deployer.deploy(MetaTransactionWallet, true);
+  await deployer.deploy(MetaTransactionWallet, false);
 };
