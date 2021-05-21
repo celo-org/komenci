6de0f1a9e35e10a6a2d2bc73bf66a66c45011b01
Gonzalo Nardini <nardinigonzalom@gmail.com>
2021-05-21 14:19:11 -0300

    Add endpoint to expose invite rewards by tx hash

---
+++ packages/apps/rewards/src/invite/inviteReward.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Query, Req } from '@nestjs/common'
+import { InviteReward } from './inviteReward.entity'
+import { InviteRewardRepository } from './inviteReward.repository'
+
+@Controller({
+  path: 'v1',
+})
+export class InviteRewardController {
+  constructor(
+    private readonly inviteRewardRepository: InviteRewardRepository,
+  ) {}
+
+  @Get('invite_reward')
+  async inviteReward(@Query('tx_hash') txHash): Promise<InviteReward> {
+    return this.inviteRewardRepository.findOne({ rewardTxHash: txHash })
+  }
+}
