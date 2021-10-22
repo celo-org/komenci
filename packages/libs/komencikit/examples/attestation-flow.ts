ffed6cead0e18312d09ace6bfb9583bfbaf4a4cc
eelanagaraj <7308464+eelanagaraj@users.noreply.github.com>
2021-10-22 18:24:36 +0200

    Only works for alfajores validators but mimics flow

--- packages/libs/komencikit/examples/attestation-flow.ts
+++ packages/libs/komencikit/examples/attestation-flow.ts
@@ -1,29 +1,38 @@
-import { base64ToHex, hexToBuffer } from '@celo/base'
+import {
+  // base64ToHex,
+  hexToBuffer
+} from '@celo/base'
 import { newKit } from '@celo/contractkit'
 import { WasmBlsBlindingClient } from '@celo/identity/lib/odis/bls-blinding-client'
 import { compressedPubKey } from '@celo/utils/lib/dataEncryptionKey'
 import { LocalWallet } from '@celo/wallet-local'
 import { randomHex } from 'web3-utils'
 import { KomenciKit } from '../src'
+import { extractAttestationCodeFromMessage } from '@celo/utils/lib/attestations'
+import { AttestationRequest, GetAttestationRequest } from '@celo/utils/lib/io'
 
 enum Env {
   local = 'local',
   alfajores_weu = 'alfajores_weu',
   alfajores_eus = 'alfajores_eus',
   rc1_br = 'rc1_br',
-  rc1_sea = 'rc1_sea',
+  rc1_sea = 'rc1_sea'
 }
 
 enum Network {
   alfajores = 'alfajores',
-  rc1 = 'rc1',
+  rc1 = 'rc1'
 }
 
 const captchaToken = process.argv[3] || 'special-captcha-bypass-token'
 const env = (process.argv[2] as Env) || Env.local
 
 let network: Network
-if (env === Env.alfajores_weu || env === Env.alfajores_eus || env === Env.local) {
+if (
+  env === Env.alfajores_weu ||
+  env === Env.alfajores_eus ||
+  env === Env.local
+) {
   network = Network.alfajores
 } else if (env === Env.rc1_br || env === Env.rc1_sea) {
   network = Network.rc1
@@ -37,11 +46,11 @@ const WALLET_IMPLEMENTATIONS: Record<Network, Record<string, string>> = {
   [Network.alfajores]: {
     '1.1.0.0-p1': '0x88a2b9B8387A1823D821E406b4e951337fa1D46D',
     '1.1.0.0-p2': '0x786ec5A4F8DCad3A58D5B1A04cc99B019E426065',
-    '1.1.0.0-p3': '0x5C9a6E3c3E862eD306E2E3348EBC8b8310A99e5A',
+    '1.1.0.0-p3': '0x5C9a6E3c3E862eD306E2E3348EBC8b8310A99e5A'
   },
   [Network.rc1]: {
-    '1.1.0.0-p2': '0x6511FB5DBfe95859d8759AdAd5503D656E2555d7',
-  },
+    '1.1.0.0-p2': '0x6511FB5DBfe95859d8759AdAd5503D656E2555d7'
+  }
 }
 
 let contractVersion = process.argv[4]
@@ -53,13 +62,14 @@ if (WALLET_IMPLEMENTATIONS[network][contractVersion] === undefined) {
   throw new Error('invalid contract version')
 }
 
-const WALLET_IMPLEMENTATION_ADDRESS = WALLET_IMPLEMENTATIONS[network][contractVersion]
+const WALLET_IMPLEMENTATION_ADDRESS =
+  WALLET_IMPLEMENTATIONS[network][contractVersion]
 
 const ODIS_PUB_KEYS: Record<Network, string> = {
   alfajores:
     'kPoRxWdEdZ/Nd3uQnp3FJFs54zuiS+ksqvOm9x8vY6KHPG8jrfqysvIRU0wtqYsBKA7SoAsICMBv8C/Fb2ZpDOqhSqvr/sZbZoHmQfvbqrzbtDIPvUIrHgRS0ydJCMsA',
   rc1:
-    'FvreHfLmhBjwxHxsxeyrcOLtSonC9j7K3WrS4QapYsQH6LdaDTaNGmnlQMfFY04Bp/K4wAvqQwO9/bqPVCKf8Ze8OZo8Frmog4JY4xAiwrsqOXxug11+htjEe1pj4uMA',
+    'FvreHfLmhBjwxHxsxeyrcOLtSonC9j7K3WrS4QapYsQH6LdaDTaNGmnlQMfFY04Bp/K4wAvqQwO9/bqPVCKf8Ze8OZo8Frmog4JY4xAiwrsqOXxug11+htjEe1pj4uMA'
 }
 
 console.log('Using wallet implementation: ', WALLET_IMPLEMENTATION_ADDRESS)
@@ -76,7 +86,7 @@ console.log('Data encryption key: ', dek)
 
 const fornoURL: Record<Network, string> = {
   alfajores: 'https://alfajores-forno.celo-testnet.org',
-  rc1: 'https://rc1-forno.celo-testnet.org',
+  rc1: 'https://rc1-forno.celo-testnet.org'
 }
 
 const contractKit = newKit(fornoURL[network], wallet)
@@ -91,17 +101,17 @@ const SERVICE_URL: Record<Env, string> = {
   alfajores_weu: 'https://weu.komenci.alfajores.celo-testnet.org',
   alfajores_eus: 'https://eus.komenci.alfajores.celo-testnet.org',
   rc1_br: 'https://br.komenci.rc1.celo-testnet.org',
-  rc1_sea: 'https://sea.komenci.rc1.celo-testnet.org',
+  rc1_sea: 'https://sea.komenci.rc1.celo-testnet.org'
 }
 
 const komenciKit = new KomenciKit(contractKit, account, {
-  url: SERVICE_URL[env],
+  url: SERVICE_URL[env]
 })
 
 const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 })
 
 const run = async () => {
@@ -119,14 +129,16 @@ const run = async () => {
   if (!checkSession.ok) {
     return
   }
-  const deployWallet = await komenciKit.deployWallet(WALLET_IMPLEMENTATION_ADDRESS)
+  const deployWallet = await komenciKit.deployWallet(
+    WALLET_IMPLEMENTATION_ADDRESS
+  )
   console.log('DeployWallet: ', deployWallet)
   if (!deployWallet.ok) {
     return
   }
   const walletAddress = deployWallet.result
   // cached:
-  const phoneNumber = '+105551234'
+  const phoneNumber = '+TODO_FILL_IN_YOUR_NUMBER'
   // const identifier = '0x59c637d9c774d242dc36bdb0445e29fa79c69b74ef70b6d1a5c407c1db6b4110'
 
   const blsBlindingClient = new WasmBlsBlindingClient(ODIS_PUB_KEY)
@@ -140,7 +152,7 @@ const run = async () => {
     return
   }
   const identifier = getIdentifier.result.identifier
-  // const pepper = getIdentifier.result.pepper
+  const pepper = getIdentifier.result.pepper
   const checkSession2 = await komenciKit.checkSession()
   console.log(checkSession2)
   if (!checkSession2.ok) {
@@ -149,17 +161,32 @@ const run = async () => {
 
   // Set Account with DEK
   console.log('Registering DEK with setAccount')
-  const setAccount = await komenciKit.setAccount(walletAddress, '', dekPublicKey, account)
+  const setAccount = await komenciKit.setAccount(
+    walletAddress,
+    '',
+    dekPublicKey,
+    account
+  )
   console.log('setAccount: ', setAccount)
   if (!setAccount.ok) {
     return
   }
 
-  const statsBefore = await attestations.getAttestationStat(identifier, walletAddress)
+  const statsBefore = await attestations.getAttestationStat(
+    identifier,
+    walletAddress
+  )
   console.log('Before ===== ')
   console.log(statsBefore)
-  const requestAttestations = await komenciKit.requestAttestations(walletAddress, identifier, 3)
-  const statsAfter = await attestations.getAttestationStat(identifier, walletAddress)
+  const requestAttestations = await komenciKit.requestAttestations(
+    walletAddress,
+    identifier,
+    3
+  )
+  const statsAfter = await attestations.getAttestationStat(
+    identifier,
+    walletAddress
+  )
   console.log('After ===== ')
   console.log(statsAfter)
   console.log('RequestAttestations: ', requestAttestations)
@@ -167,13 +194,19 @@ const run = async () => {
     return
   }
 
-  const events = await attestations.getPastEvents(attestations.eventTypes.AttestationsRequested, {
-    fromBlock: requestAttestations.result.blockNumber,
-    toBlock: requestAttestations.result.blockNumber,
-  })
+  const events = await attestations.getPastEvents(
+    attestations.eventTypes.AttestationsRequested,
+    {
+      fromBlock: requestAttestations.result.blockNumber,
+      toBlock: requestAttestations.result.blockNumber
+    }
+  )
   console.log(events)
 
-  const selectIssuers = await komenciKit.selectIssuers(walletAddress, identifier)
+  const selectIssuers = await komenciKit.selectIssuers(
+    walletAddress,
+    identifier
+  )
   if (!selectIssuers.ok) {
     return
   }
@@ -182,7 +215,7 @@ const run = async () => {
     attestations.eventTypes.AttestationIssuerSelected,
     {
       fromBlock: selectIssuers.result.blockNumber,
-      toBlock: selectIssuers.result.blockNumber,
+      toBlock: selectIssuers.result.blockNumber
     }
   )
   console.log(issuersEvents)
@@ -193,16 +226,37 @@ const run = async () => {
   )
   console.log(attestationsToComplete)
   console.log('====================')
-  // const possibleErrors = await requestAttestationsFromIssuers(
-  //   attestationsToComplete,
-  //   attestations,
-  //   phoneNumber,
-  //   walletAddress,
-  //   pepper
-  // )
-  // console.log(possibleErrors)
+  // NOTE: this is currently a janky way of getting a prefix code for alfajores
+  const getSecurityPrefix = (attestationToComplete: any) =>
+    attestationToComplete.name[10]
+
+  const postAttestationRequest = async (attestationToComplete: any) => {
+    const requestBody: AttestationRequest = {
+      phoneNumber,
+      account: walletAddress,
+      issuer: attestationToComplete.issuer,
+      salt: pepper,
+      smsRetrieverAppSig: undefined,
+      language: 'en',
+      securityCodePrefix: getSecurityPrefix(attestationToComplete)
+    }
+    console.log(requestBody)
+    const response = await attestations.revealPhoneNumberToIssuer(
+      attestationToComplete.attestationServiceURL,
+      requestBody
+    )
+    return response.json()
+  }
+  console.log(
+    'Responses: ',
+    await Promise.all(attestationsToComplete.map(postAttestationRequest))
+  )
+
   while (true) {
-    attestationsToComplete = await attestations.getActionableAttestations(identifier, walletAddress)
+    attestationsToComplete = await attestations.getActionableAttestations(
+      identifier,
+      walletAddress
+    )
 
     if (attestationsToComplete.length === 0) {
       break
@@ -216,40 +270,70 @@ const run = async () => {
         if (base64Code === 'exit') {
           process.exit(0)
         }
-        const code = base64ToHex(base64Code)
-        const matchingIssuer = await attestations.findMatchingIssuer(
-          identifier,
-          walletAddress,
-          code,
-          attestationsToComplete.map((a) => a.issuer)
-        )
 
-        if (matchingIssuer === null) {
-          console.warn('No matching issuer found for code')
-          resolve(null)
-          return
+        const prefix = base64Code.substring(0, 1)
+        const respondingService = attestationsToComplete.filter(
+          (attestationToComplete: any) =>
+            getSecurityPrefix(attestationToComplete).toString() === prefix
+        )
+        if (respondingService.length === 1) {
+          const getAttestationRequest: GetAttestationRequest = {
+            account: walletAddress,
+            issuer: respondingService[0].issuer,
+            phoneNumber: phoneNumber,
+            salt: pepper,
+            securityCode: base64Code.substring(1)
+          }
+          try {
+            const attestation = await attestations.getAttestationForSecurityCode(
+              respondingService[0].attestationServiceURL,
+              getAttestationRequest,
+              account
+            )
+            console.log('Attestation: ', attestation)
+            const code = extractAttestationCodeFromMessage(attestation)
+            if (code) {
+              console.log('Extracted code: ', code)
+              const matchingIssuer = await attestations.findMatchingIssuer(
+                identifier,
+                walletAddress,
+                code,
+                attestationsToComplete.map((a: any) => a.issuer)
+              )
+              if (matchingIssuer === null) {
+                console.warn('No matching issuer found for code')
+                resolve(null)
+                return
+              }
+              const isValidRequest = await attestations.validateAttestationCode(
+                identifier,
+                walletAddress,
+                matchingIssuer,
+                code
+              )
+              if (!isValidRequest) {
+                console.warn('Code was not valid')
+                resolve(null)
+                return
+              }
+              const completeResult = await komenciKit.completeAttestation(
+                walletAddress,
+                identifier,
+                matchingIssuer,
+                code
+              )
+              console.log(completeResult)
+            } else {
+              console.error('extracted code is null')
+            }
+          } catch (error) {
+            console.error(error)
+          }
+        } else {
+          console.log('respondingService:', respondingService)
+          console.log('Prefix does not match any issuers')
         }
 
-        const isValidRequest = await attestations.validateAttestationCode(
-          identifier,
-          walletAddress,
-          matchingIssuer,
-          code
-        )
-        if (!isValidRequest) {
-          console.warn('Code was not valid')
-          resolve(null)
-          return
-        }
-
-        const completeResult = await komenciKit.completeAttestation(
-          walletAddress,
-          identifier,
-          matchingIssuer,
-          code
-        )
-
-        console.log(completeResult)
         resolve(null)
       })
     })
